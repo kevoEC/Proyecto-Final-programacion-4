8e41df91cd5546505b85ebf8a1452285bf8a2544
Kevin Rosero <kevin.rosero@udla.edu.ec>
2023-02-01 00:27:01 -0500

    "Se agrega un modelo MVVM extra para verificar la actualizacion de los menus"

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalProgramacion4.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        bool isBusy;
+        string title;
+        public bool IsBusy { 
+        
+            get=>isBusy;
+            set
+            {
+                if (isBusy == value)
+                    return;
+                isBusy = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value) 
+                    return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsNotBusy => !isBusy;
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string name=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
